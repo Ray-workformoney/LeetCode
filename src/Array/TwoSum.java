fa764c8b99c3d02217676c9235731dd995dcbc52
Rui <13821927613@outlook.com>
2019-04-02 17:18:05 +0800

    TwoSum

---
+++ src/Array/TwoSum.java
@@ -0,0 +1,48 @@
+/*给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]*/
+package Array;
+
+import java.util.*;
+
+public class TwoSum 
+{
+	public static int[] Search(int[] nums, int target)
+	{
+		int[] res = new int[2];
+		boolean find = false;
+		//Map存储<与target的差值, 数组下标>
+		Map<Integer, Integer> dif = new HashMap<Integer, Integer>();
+		for (int i = 0; i < nums.length; i++)
+		{
+			if (dif.get(nums[i]) != null)
+			{
+				res[0] = dif.get(nums[i]);
+				res[1] = i;
+				find = true;
+				break;
+			}
+			dif.put(target - nums[i], i);
+		}
+		if (!find)
+		{
+			res[0] = -1;
+			res[1] = -1;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) 
+	{
+		int[] nums = {2, 7, 11, 15};
+		int target = 9;
+		int[] res = new int[2];
+		res = TwoSum.Search(nums, target);
+		for (int i = 0; i < res.length; i++)
+			System.out.print(res[i] + " ");
+	}
+
+}
