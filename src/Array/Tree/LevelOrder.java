ccc279312dab2dfb790e14abc2d62487fb1f50d1
huangrui <13821927613@outlook.com>
2021-11-01 18:08:33 +0800

    [102]二叉树的层序遍历

---
+++ src/Array/Tree/LevelOrder.java
@@ -0,0 +1,59 @@
+package Array.Tree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+/**
+ * [102]二叉树的层序遍历
+ * @author : huangrui
+ * @version :
+ * @date : 2021-11-01 17:44
+ **/
+public class LevelOrder {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {
+        }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<List<Integer>> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    level.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
