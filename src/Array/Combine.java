659fa2110a9b5e1d8fde3b4a4f5c0f390bdecaa8
huangrui <13821927613@outlook.com>
2021-12-16 17:08:32 +0800

    [77]组合

---
+++ src/Array/Combine.java
@@ -0,0 +1,50 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import com.huangrui.leetcode.PrintUtils;
+/**
+ * [77]组合
+ * @author : huangrui
+ * @version :
+ * @date : 2021-12-15 19:34
+ **/
+public class Combine {
+
+    public List<List<Integer>> combine(int n, int k) {
+
+        int[] nums = new int[n];
+        for (int i = 1; i <= n; i++) {
+            nums[i - 1] = i;
+        }
+        List<List<Integer>> r = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Integer> combine = new ArrayList<>();
+            combine.add(nums[i]);
+            dfs(Arrays.copyOfRange(nums, i + 1, nums.length), k, combine, r);
+        }
+        return r;
+    }
+
+    private void dfs(int[] nums, int k, List<Integer> combine, List<List<Integer>> result) {
+
+        if (combine.size() == k) {
+            result.add(combine);
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> newCombine = new ArrayList<>(combine);
+            newCombine.add(nums[i]);
+            dfs(Arrays.copyOfRange(nums, i + 1, nums.length), k, newCombine, result);
+        }
+    }
+
+    public static void main(String[] args) {
+        Combine combine = new Combine();
+        PrintUtils.print(combine.combine(4, 2));
+    }
+}
