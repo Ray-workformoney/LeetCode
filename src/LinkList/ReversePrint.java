fccbda5afdd356d33d9389b96b335ec5065284b3
huangrui <13821927613@outlook.com>
2022-01-10 15:40:13 +0800

    [剑指offer06]从尾到头打印链表

---
+++ src/LinkList/ReversePrint.java
@@ -0,0 +1,29 @@
+package LinkList;
+
+import java.util.Stack;
+import com.huangrui.leetcode.ListNode;
+/**
+ * [剑指offer06]从尾到头打印链表
+ * @author : huangrui
+ * @version :
+ * @date : 2022-01-10 15:22
+ **/
+public class ReversePrint {
+
+    public int[] reversePrint(ListNode head) {
+
+        Stack<Integer> stack = new Stack<>();
+        int len = 0;
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+            len++;
+        }
+        int[] res = new int[len];
+        for (int i = 0; !stack.isEmpty(); i++) {
+            int n = stack.pop();
+            res[i] = n;
+        }
+        return res;
+    }
+}
