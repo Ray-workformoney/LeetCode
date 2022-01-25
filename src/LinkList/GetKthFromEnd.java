abd30123cca8918b44af1ea4042c5e8a451c9484
huangrui <13821927613@outlook.com>
2022-01-25 16:00:23 +0800

    [剑指offer21]调整数组顺序使奇数位于偶数前面

---
+++ src/LinkList/GetKthFromEnd.java
@@ -0,0 +1,25 @@
+package LinkList;
+
+import com.huangrui.leetcode.ListNode;
+/**
+ * [剑指offer22]链表中倒数第k个节点
+ * @author : huangrui
+ * @version :
+ * @date : 2022-01-25 15:40
+ **/
+public class GetKthFromEnd {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast!= null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+}
