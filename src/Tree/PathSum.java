81f9d6f77fce9e41d5d3f6851e118b5546f40fea
huangrui <13821927613@outlook.com>
2022-02-10 21:29:36 +0800

    [剑指offer34]二叉树中和为某一值的路径

---
+++ src/Tree/PathSum.java
@@ -0,0 +1,41 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.huangrui.leetcode.TreeNode;
+/**
+ * [剑指offer34]二叉树中和为某一值的路径
+ * @author : huangrui
+ * @version :
+ * @date : 2022-02-10 21:04
+ **/
+public class PathSum {
+
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (root != null) {
+            List<Integer> path = new ArrayList<>();
+            preOrder(root, res, path, 0, target);
+        }
+        return res;
+    }
+
+    private void preOrder(TreeNode node, List<List<Integer>> res, List<Integer> path, int sum, int target) {
+
+        sum += node.val;
+        List<Integer> thisPath = new ArrayList<>(path);
+        thisPath.add(node.val);
+        if (node.left != null) {
+            preOrder(node.left, res, thisPath, sum, target);
+        }
+        if (node.right != null) {
+            preOrder(node.right, res, thisPath, sum, target);
+        }
+        if (node.left == null && node.right == null) {
+            if (sum == target) {
+                res.add(thisPath);
+            }
+        }
+    }
+}
