258139ec219e5cd66b8ce29f5f3fd5da02d6ee7b
huangrui <13821927613@outlook.com>
2022-02-17 16:34:53 +0800

    [206] Reverse Linked List

---
+++ src/LinkList/ReverseList2.java
@@ -0,0 +1,33 @@
+package LinkList;
+
+import com.huangrui.leetcode.ListNode;
+/**
+ * [206] Reverse Linked List
+ * @author : huangrui
+ * @version :
+ * @date : 2022-02-17 16:29
+ **/
+public class ReverseList2 {
+
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null, itr = head, next;
+        while (itr != null) {
+            if (pre == null) {
+                // 第一个节点
+                pre = itr;
+                itr = itr.next;
+                pre.next = null;
+            } else {
+                next = itr.next;
+                itr.next = pre;
+                pre = itr;
+                itr = next;
+            }
+        }
+        return pre;
+    }
+}
